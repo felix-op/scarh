999b0b643f0c7541148885109976eadf09713c67
Bruno <bruno1692000@gmail.com>
2025-10-25 01:04:36 -0300

    se agrego la libreria de simplejwt y los endpoints de sesiones (ref #7)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .views import hola_api
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from .views import LogoutView
 
 urlpatterns = [
     path('hola/', hola_api),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
+
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()  # Agrega el token a la lista negra
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 def hola_api(request):
