1c0be572c92bbc44ef66238d2c6d446404761353
pablodonato2001 <pablodonato2001@gmail.com>
2025-12-02 12:02:48 -0300

    arreglos por los cambios de version

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
 	images: {
 		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840, 5000],
 	},
 };
 
 export default nextConfig;
+
