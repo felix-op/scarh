4d6b051e0ebf57bea3ef125bc344e1bee1c11cf4
felix-op <felixoscarperezsegovia@gmail.com>
2025-12-03 22:48:52 -0300

    feat: se agrega un boton con variantes e iconos. Se agregan iconos para el CampoInput

--- frontend/shared/componentes/campos/CampoInput.tsx
+++ frontend/shared/componentes/campos/CampoInput.tsx
@@ -1,3 +1,8 @@
+type EndDecorationsOptions = {
+	className: string;
+	onClick?: () => void;
+};
+
 type CampoInputProps = {
     id?: string;
     label: string;
@@ -5,32 +10,52 @@ type CampoInputProps = {
 	type: string;
 	placeholder: string;
 	className?: string;
+	icon?: string;
     value: string;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	disabled?: boolean;
+	endDecorations?: EndDecorationsOptions[];
 };
 
 export default function CampoInput({
-    id,
-    label,
-    name,
+	id = '',
+	label,
+	name,
 	type,
 	placeholder,
-	className,
-    value,
-    onChange,
+	className = '',
+	icon = '',
+	value,
+	onChange,
+	disabled = false,
+	endDecorations = [],
 }: CampoInputProps) {
 	return (
-        <div className="flex flex-col gap-2">
-            <label className="text-base font-medium text-[#1E1E1E] mb-2" htmlFor={id || name}>{label}</label>
-            <input
-                className={`w-full p-3 px-4 bg-white rounded-lg border border-border outline-none text-base text-[#1E1E1E] ${className || ''}`}
-                id={id || name}
-                type={type}
-                value={value}
-                name={name}
-                onChange={onChange}
-                placeholder={placeholder}
-            />
-        </div>
+		<div className="flex flex-col gap-2">
+			<label className="text-base font-medium text-[#1E1E1E]" htmlFor={id || name}>{label}</label>
+			<div className="relative flex flex-row items-center">
+				<span className={`absolute left-3 text-2xl ${icon}`} />
+				<input
+					className={`w-full p-3 px-4 ${icon ? 'pl-10' : ''} ${disabled ? 'bg-gray-400' : 'bg-white'} rounded-lg border border-border outline-none text-base text-[#1E1E1E] ${className}`}
+					id={id || name}
+					type={type}
+					value={value}
+					name={name}
+					onChange={onChange}
+					placeholder={placeholder}
+					disabled={disabled}
+				/>
+
+				{endDecorations.map((decoration, index) => {
+					return (
+						<span
+							key={`${name}-decoration-${index}`}
+							className={`absolute right-3 text-2xl ${decoration.className}`}
+							onClick={decoration.onClick}
+						/>
+					);
+				})}
+			</div>
+		</div>
 	);
 }
