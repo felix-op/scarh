f269b352f9866cdfa49d58dc82f64cc3133c922b
felix-op <felixoscarperezsegovia@gmail.com>
2026-02-21 13:25:14 -0300

    feat: se agrega estado de error a Textfield y se agregan estilos de hover y active al Textfield y Selector (ref #57)

--- frontend/shared/componentes/campos/TextField.tsx
+++ frontend/shared/componentes/campos/TextField.tsx
@@ -1,7 +1,8 @@
-import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import { InputProps } from "types/campos";
 
-type TextFieldProps = {
+export type TextFieldProps = {
 	icon?: string;
+	error?: boolean;
 	disabled?: boolean;
 	isLoading?: boolean;
 	endDecoration?: {
@@ -12,19 +13,23 @@ type TextFieldProps = {
 
 export default function TextField({
 	icon,
+	error = false,
 	endDecoration,
 	disabled = false,
 	isLoading = false,
-}: DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & TextFieldProps) {
+	...rest
+}: InputProps & TextFieldProps) {
 	return (
 		<div className="relative flex flex-row items-center">
 			<span className={`absolute left-3 text-2xl ${icon}`} />
 			<input
+				{...rest}
 				className={`
 					w-full p-3 px-4 text-base text-foreground
-					rounded-lg border border-border outline-none
+					rounded-lg border-2 outline-none
 					${!!icon ? 'pl-10' : ''}
 					${isLoading || disabled ? 'bg-campo-input-disabled cursor-not-allowed' : 'bg-campo-input'}
+					${error ? 'border-red-500' : 'border-border hover:border-foreground focus:border-principal'}
 					${endDecoration ? 'pr-10' : ''}
 				`}
 			/>
