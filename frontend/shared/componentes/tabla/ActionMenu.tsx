03ef341e919288c8786a989b64ca6386918741a7
felix-op <felixoscarperezsegovia@gmail.com>
2026-02-24 21:23:46 -0300

    refactor: se corrige el padding en ActionMenu

--- frontend/shared/componentes/tabla/ActionMenu.tsx
+++ frontend/shared/componentes/tabla/ActionMenu.tsx
@@ -8,7 +8,7 @@ export default function ActionMenu({
 	children
 }: ActionMenuProps) {
 	return (
-		<div className="flex justify-start pl-4 items-center w-full h-full">
+		<div className="flex justify-start pl-4 items-center w-full h-full gap-2">
 			{children}
 		</div>
 	);
