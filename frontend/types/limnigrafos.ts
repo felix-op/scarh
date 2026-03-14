1c8b03fbf66981bc70a1e010b43961fe868513ed
felix-op <felixoscarperezsegovia@gmail.com>
2026-03-14 10:28:57 -0300

    refactor: se mueven los endpoints de limnigrafos en un archivo aparte

---
+++ frontend/types/limnigrafos.ts
@@ -0,0 +1,63 @@
+export type Ubicacion = {
+    id: number
+    longitud: number
+    latitud: number
+    nombre: string
+}
+
+export type EstadoLimnigrafo =
+  | "activo"
+  | "prueba"
+  | "fuera"
+  | "peligro"
+  | "advertencia";
+
+export type LimnigrafoResponse = {
+    id: number
+    codigo: string
+    estado: EstadoLimnigrafo
+    descripcion: string
+    ultimo_mantenimiento: string
+    ultima_medicion: string
+    tipo_comunicacion: string[]
+    tiempo_advertencia: string
+    tiempo_peligro: string
+    ultima_conexion: string
+    bateria_max: number
+    bateria_min: number
+    bateria: number
+    memoria: number
+    ubicacion: Ubicacion
+}
+
+export type LimnigrafoPostRequest = {
+    codigo: string
+    memoria?: number | null
+    tipo_comunicacion?: string[] | null
+}
+
+export type LimnigrafoPutRequest = {
+    codigo?: string
+    descripcion?: string
+    ultimo_mantenimiento?: string
+    tipo_comunicacion?: string[]
+    bateria_max?: number
+    bateria_min?: number
+    memoria?: number
+    tiempo_advertencia?: string
+    tiempo_peligro?: string
+    ubicacion_id?: string
+}
+
+export type LimnigrafoPatchtRequest = {
+    codigo?: string
+    descripcion?: string
+    ultimo_mantenimiento?: string
+    tipo_comunicacion?: string[]
+    bateria_max?: number
+    bateria_min?: number
+    memoria?: number
+    tiempo_advertencia?: string
+    tiempo_peligro?: string
+    ubicacion_id?: string
+}
